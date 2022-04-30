fbbc15fd2ad6074b2a76104d473dd67f20d78eb6
antonio-datahack <antonio.datahack@gmail.com>
2022-04-30 14:16:41 +0200

    44 object methods

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -253,7 +253,7 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]),calcTip(bills[2])]
 const totals = [(tips[0]+bills[0]), (tips[1]+bills[1]),(tips[2]+bills[2])]
 console.log(bills, tips, totals);
 
-*/
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 // 42. Introduction to Objects
@@ -281,3 +281,63 @@ console.log(jonas['lastName']);
 const nameKey = 'Name';
 console.log(jonas['first' + nameKey]);
 console.log(jonas['last' + nameKey]);
+
+const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends');
+console.log(jonas[interestedIn]);
+
+// if(jonas[interestedIn]) {
+//   console.log(jonas[interestedIn]);
+// } else {
+//   console.log('Wrong request! Choose between firstName, lastName, age, job, and friends')
+// }
+
+if(jonas[interestedIn]) {
+  console.log(jonas[interestedIn]);
+} else {
+  const interestedIn2 = prompt('Wrong request! Choose between firstName, lastName, age, job, and friends');
+  console.log(jonas[interestedIn2]);
+}
+
+jonas.location = 'Portugal';
+jonas['Twitter'] = '@jonasschmedtman';
+console.log(jonas);
+
+
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
+
+*/
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// 44. Object Methods
+
+
+const jonas = {
+  firstName: 'Jonas', 
+  lastName: 'Schmedmann',
+  birthYeah: 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven'],
+  hasDriversLicense: true,
+
+  // calcAge: function(birthYeah) {
+  //   return 2022 - birthYeah
+  // }
+
+  // calcAge: function() {
+  //   console.log(this) // 'this' point to jonas
+  //   return 2022 - this.birthYeah
+  // }
+
+  calcAge: function() {
+    this.age = 2022 - this.birthYeah
+    return this.age;
+  }
+};
+
+console.log(jonas.calcAge());
+
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+
+// console.log(jonas['calcAge'](1991));
