1dd22508d9c61444bec94b0302ae4e8bbf6bc131
antonio-datahack <antonio.datahack@gmail.com>
2022-05-02 21:50:41 +0200

    modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+const closeModal = function() {
+  console.log('Button Clicked');
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+const openModal = function() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+for(let i = 0; i < btnsOpenModal.length; i++) 
+  btnsOpenModal[i].addEventListener('click', openModal);
+  btnCloseModal.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
+  
