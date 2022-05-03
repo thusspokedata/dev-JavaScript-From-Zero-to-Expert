9c8f5d50d6e81c73f6d44f19037705caef24b36d
antonio-datahack <antonio.datahack@gmail.com>
2022-05-03 22:45:18 +0200

    added pig game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,48 @@
 'use strict';
+
+// Selecting elements
+const player0El = document.querySelector('.player--0')
+const player1El = document.querySelector('.player--1')
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1'); // it suppose to be faster than a querySelector
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold= document.querySelector('.btn--hold');
+
+// let diceNumber = Math.trunc(Math.random()*6)+1;
+
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function() {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random()*6)+1;
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  // 3. Check for rolled 1: if True, switch to next player
+  if(dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+  } else {
+    // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
+});
