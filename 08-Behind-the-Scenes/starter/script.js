9a8a1a8a273a2a2a53ea7deb73ae6d2b9736d118
antonio-datahack <antonio.datahack@gmail.com>
2022-05-06 00:56:28 +0200

    added Destructuring Arrays and Objects

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,40 @@
 'use strict';
+///////////////////////////////////////
+// Primitives vs. Objects in Practice
+
+// Primitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+// Reference types
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage:', jessica);
+console.log('After marriage: ', marriedJessica);
+// marriedJessica = {};
+
+// Copying objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before marriage:', jessica2);
+console.log('After marriage: ', jessicaCopy);
+*/
+
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,14 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +35,93 @@ const restaurant = {
     },
   },
 };
+
+/*
+////////////////////////////////////////////////////////////////////////////////////////
+// 103. Destructuring Arrays
+
+const arr = [2,3,4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x,y,z] = arr;
+console.log(x);
+console.log(arr)
+const [first, ,third] = restaurant.categories;
+console.log(third)
+
+
+let [main, ,secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// switching variables
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+[main, secondary] = [secondary, main]
+console.log(main, secondary);
+
+console.log(restaurant.order(2,0));
+
+// Receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2,0)
+console.log(starter, mainCourse)
+
+// nested destructuring
+const nested = [2,4,[5,6]]
+// const [i,,j] = nested
+// console.log(i,j)
+const [i,,[j,k]] = nested
+console.log(i,j,k);
+
+// default values
+const [p=1,q=1,r=1, s=1] = [8,9]
+console.log(p,q,r,s);
+
+*/
+
+//////////////////////////////////////////////////////////////////////////////
+// 104. Destructuring Objects
+
+const {name, openingHours, categories} = restaurant;
+console.log(name, openingHours, categories);
+
+
+// const {name: restaurantName, openingHours: hours, categories: tags} = restaurant; // esta fucnion nos permite obtener el mismo resultado pero con diferente nombre de variables
+// console.log(restaurantName, hours, tags);
+
+// dafault values
+const {menu = [], starterMenu: starters = []} =  restaurant; // le damos un valor un default value en caso de q no exista
+console.log(menu, starters)
+
+// mutating variables
+let a = 111;
+let b = 999;
+const obj = {a:23, b:7, c:14};
+({a,b} = obj);
+console.log(a,b)
+
+// nested objects
+const { fri } = openingHours; // puedo usarlo porque openingHours está en una variable en la funcion del renglon 85
+console.log(fri);
+
+// const { fri: {open, close} } = openingHours; // puedo usarlo porque openingHours está en una variable en la funcion del renglon 85
+// console.log(open, close);
+
+const { fri: {open: o, close: c} } = openingHours; // puedo usarlo porque openingHours está en una variable en la funcion del renglon 85
+console.log(o, c);
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21', 
+  mainIndex: 2, 
+  starterIndex: 2,
+})
+
+restaurant.orderDelivery({
+  address: 'Bluecherstr, 21', 
+  starterIndex: 2,
+})
