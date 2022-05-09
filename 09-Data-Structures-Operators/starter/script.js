08e91c1d7763e6659421700441d9a60f8f33bac8
antonio-datahack <antonio.datahack@gmail.com>
2022-05-09 23:42:13 +0200

    added 129 and image

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -900,10 +900,42 @@ const capitalizeName = function(name) {
   const names = name.split(' ');
   const namesUpper = [];
   for (const n of names) {
-    namesUpper.push(n[0].toUpperCase() + n.slice(1));
+  //   namesUpper.push(n[0].toUpperCase() + n.slice(1)); // one way
+  namesUpper.push(n.replace(n[0], n[0].toUpperCase()));  // another way ...nice one!
   }
+
   console.log(namesUpper.join(' '));
 }
 
 capitalizeName('jessica and smith davis');
-capitalizeName('jonas schmedtmann')
+capitalizeName('jonas schmedtmann');
+
+// Padding
+const message = 'Go to gate 23!'
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+console.log('Jonas'.padStart(20, '+').padEnd(30, '+'));
+
+// ##############################################
+// ########### muy buena!!! #####################
+// ##############################################
+const masCreditCard = function(number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*')
+}
+
+console.log(masCreditCard(1304865465));
+console.log(masCreditCard(15151513135131304865465));
+console.log(masCreditCard('15151513135131304865465'));
+
+// Repeat
+const message2 = 'Bad weather... All Departures Delayed...\n';
+console.log(message2.repeat(5))
+
+const planesInLine = function(n) {
+  console.log(`There are ${n} planes in line ${'🛫'.repeat(n)}`);
+}
+
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,58 @@
 'use strict';
+/*
+const bookings = [];
+
+const createBooking = function (flightNum, 
+  numPassenger = 1, 
+  price = 199 * numPassenger) {
+  // ES5
+  // numPassenger = numPassenger || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassenger,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 1000);
+*/
+///////////////////////////////////////////////////////////////////////////////////////////
+// 129. How Passing Arguments Works: Value vs. Reference
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jose de San Martin',
+  passport: 23483738373
+}
+
+const checkIn = function(flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name
+
+  if(passenger.passport === 23483738373) {
+    alert('Checked In')
+  } else {
+    alert('Wrong passport!')
+  }
+}
+
+// checkIn(flight, jonas)
+// console.log(flight);
+// console.log(jonas);
+
+const newPassport = function(person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+}
+
+newPassport(jonas)
+checkIn(flight, jonas)
+
+//javascript does not have passing by reference, only passing by value
