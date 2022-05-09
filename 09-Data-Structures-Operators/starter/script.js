27a24598910a28ccbd53cf9ce9dc4bf99e1a6bdf
antonio-datahack <antonio.datahack@gmail.com>
2022-05-09 13:30:18 +0200

    121-123

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -729,7 +729,7 @@ console.log([...question.values()]);
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 // 119. Summary: Which Data Structure to Use?
 ,
-*/
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 // 120. Coding Challenge #3
 
@@ -775,3 +775,135 @@ for (const [time, event] of gameEvents) {
   const half = time <= 45 ? 'FIRST' : 'SECOND';
   console.log(`[${half} HALF] ${time}: ${event}`);
 } 
+
+
+///////////////////////////////////////////////////////////////////////////////////
+// 121. Working With Strings - Part 1
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+console.log(plane[0]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r')); // the posicion number of 'r'
+console.log(airline.lastIndexOf('Portugal'));
+console.log(airline.lastIndexOf('portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -4));
+
+const checkMiddleSeat = function(seat) {
+  const s = seat.slice(-1);
+  if(s === 'B' || s === 'E')
+  console.log('You got the middle seat');
+  else console.log('You got lucky');
+}
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+console.log(new String('jonas'));
+console.log(typeof new String('jonas'));
+console.log(typeof new String('jonas').slice(1));
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+// 122. Working With Strings - Part 2
+
+const airline = 'TAP Air Portugal';
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+console.log('jose de san martin'.toUpperCase());
+
+// fix capitalization in name
+const passenger = 'jOnAs';
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+// Check Emails
+const email = 'hello@jonas.io';
+const loginEmail = ' Hello@Jonas.Io \n';
+const trimmedEmail = loginEmail.trim();
+console.log(trimmedEmail);
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(normalizedEmail === email);
+
+// replacing
+const priceGB = '288,97§';
+const priceUS = priceGB.replace('§', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement = 'All passengers come to boarding door 23. Boarding door 23!';
+
+console.log(announcement.replace('door', 'gate'));
+console.log(announcement.replaceAll('door', 'gate'));
+
+console.log('--------------------using REGEX---------------------');
+// using Regex
+console.log(announcement.replace(/door/g, 'gate'));
+console.log('--------------------Booleans------------------------');
+// Booleans
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Air'));
+
+if(plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the new Airbus family');
+}
+
+const checkBaggage = function(items){
+  const baggage = items.toLowerCase();
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on board');
+  } else {
+    console.log('Welcome aboard!');
+  }
+}
+
+checkBaggage('I have a laptop, some Food and a pocker Knife');
+checkBaggage('Socke and camera');
+checkBaggage('Got some snacks and a gun for protection')
+*/
+///////////////////////////////////////////////////////////////////////////////////
+// 123. Working With Strings - Part 3
+
+console.log('-------------split and join----------------------');
+
+console.log('a+very+nice+string'.split('+'));
+console.log('Jose de San Martin'.split(' '));
+
+const [firstName, lastName] = 'Jose Pedrin'.split(' ');
+console.log(firstName);
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ')
+console.log(newName);
+
+console.log('----------------------------------');
+const capitalizeName = function(name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+  for (const n of names) {
+    namesUpper.push(n[0].toUpperCase() + n.slice(1));
+  }
+  console.log(namesUpper.join(' '));
+}
+
+capitalizeName('jessica and smith davis');
+capitalizeName('jonas schmedtmann')
