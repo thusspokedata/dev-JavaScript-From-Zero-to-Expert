d2cd74d336ee2f2defcc829d30404d74c94da023
Antonio Saleme <79271990+thusspokedata@users.noreply.github.com>
2022-10-24 10:19:05 +0200

    dom chapter 190

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -15,6 +15,7 @@ const openModal = function (e) {
 };
 
 const closeModal = function () {
+  e.preventDefault();
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
@@ -42,21 +43,22 @@ console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
 
-const header = document.querySelector('.header')
-const allSections = document.querySelectorAll('.section')
-console.log(allSections) // NodeList(4) [section#section--1.section, section#section--2.section, section#section--3.section, section.section.section--sign-up]
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections); // NodeList(4) [section#section--1.section, section#section--2.section, section#section--3.section, section.section.section--sign-up]
 
 document.getElementById('section--1');
 const allButtons = document.getElementsByTagName('button');
-console.log(allButtons)
+console.log(allButtons);
 
-console.log(document.getElementsByClassName('btn')) 
+console.log(document.getElementsByClassName('btn'));
 
 // Creating and inserting elements
-const message= document.createElement('div');
-message.classList.add('cookie-message') // dentro de la clase en div va a agregar cookie-message
+const message = document.createElement('div');
+message.classList.add('cookie-message'); // dentro de la clase en div va a agregar cookie-message
 // message.textContent = 'We use cookied for improved functionality and analitycs.'
-message.innerHTML = 'We use cookied for improved functionality and analitycs. <button class="btn btn--close-cookie">Got it!</button>';
+message.innerHTML =
+  'We use cookied for improved functionality and analitycs. <button class="btn btn--close-cookie">Got it!</button>';
 
 // header.prepend(message); // pone el mensaje arriba
 header.append(message); // abajo en el header
@@ -66,15 +68,16 @@ header.append(message); // abajo en el header
 // header.after(message)
 
 // DELETE ELEMENTS
-document.querySelector('.btn--close-cookie').addEventListener('click', function(){
-  // message.remove();
-  message.parentElement.removeChild(message);
-})
-
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    // message.remove();
+    message.parentElement.removeChild(message);
+  });
 
 ///////////////////////////////////////
 // 187. Styles, Attributes and Classes
-  
+
 // Styles
 message.style.backgroundColor = '#37383d';
 message.style.width = '120%';
@@ -121,9 +124,53 @@ logo.classList.contains('c'); // not includes
 // Don't use
 logo.clasName = 'jonas';
 
+// console.log(document.documentElement);
 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
 
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  // saber ancho y alto de la pantalla en la q estoy trabajando
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // HACER SCROLL TO tres formas diferentes:
+  // Scrolling;
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+///////////////////////////////////////
+// Types of Events and Event Handlers
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 
 /*
 
@@ -140,61 +187,6 @@ const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
 
-///////////////////////////////////////
-// Modal window
-
-const openModal = function (e) {
-  e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
-
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
-
-btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
-
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
-});
-
-///////////////////////////////////////
-// Button scrolling
-btnScrollTo.addEventListener('click', function (e) {
-  const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
-
-  console.log(e.target.getBoundingClientRect());
-
-  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
-
-  console.log(
-    'height/width viewport',
-    document.documentElement.clientHeight,
-    document.documentElement.clientWidth
-  );
-
-  // Scrolling
-  // window.scrollTo(
-  //   s1coords.left + window.pageXOffset,
-  //   s1coords.top + window.pageYOffset
-  // );
-
-  // window.scrollTo({
-  //   left: s1coords.left + window.pageXOffset,
-  //   top: s1coords.top + window.pageYOffset,
-  //   behavior: 'smooth',
-  // });
-
-  section1.scrollIntoView({ behavior: 'smooth' });
-});
 
 ///////////////////////////////////////
 // Page navigation
@@ -636,4 +628,3 @@ window.addEventListener('beforeunload', function (e) {
   e.returnValue = '';
 });
 */
-
